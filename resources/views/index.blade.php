072ace17a825a4fd9544ebe4f8e6987aa6d1010b
catanielena <catani.elena1@gmail.com>
2021-11-05 16:47:19 +0100

    Add: main

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layout.main')
 @section('title', 'Homepage')
 @section('mainContent')
-    {{-- code --}}
+    <div class="container">
+        <h3 class="comics__title">Current series</h3>
+        <ul class="comics__list">
+            @foreach ($comics as $item)
+            <li class="list__item">
+                <div class="item__img">
+                    <img src="{{$item['thumb']}}" alt="{{$item['title']}}">
+                </div>
+                <h5 class="item__title">{{$item['title']}}</h5>
+            </li>
+            @endforeach
+        </ul>
+        <div class="main__btn">
+            <button type="button" class="btn btn--fill btn--padding--lg">load more</button>
+        </div>
+    </div>
 @endsection
